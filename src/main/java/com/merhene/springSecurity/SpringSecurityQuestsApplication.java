1bbb28dc5e87313335eb2002ac77b07a69280e7a
Merwan SI TAYEB <merwan.st@gmail.com>
2024-10-08 14:32:15 +0200

    initial main commit

---
+++ src/main/java/com/merhene/springSecurity/SpringSecurityQuestsApplication.java
@@ -0,0 +1,13 @@
+package com.merhene.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityQuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityQuestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merhene/springSecurity/SpringSecurityQuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merhene.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityQuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
